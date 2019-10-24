b8a294c0093550bb45bdf0f3f457c84939b0584e
Ria Gupta <ria18405@iiitd.ac.in>
2019-10-24 21:47:16 +0530

    Update use.c

--- use.c
+++ use.c
@@ -1,12 +1,14 @@
-﻿#include <stdio.h>
+//test code in the userspace directory
+
+#include <stdio.h>
 #include <linux/kernel.h>
 #include <sys/syscall.h>
 #include <unistd.h>
 int main()
 {
-int pid=3;
+int pid=3;                 
 if(pid<0){
-printf("pid negative");
+printf("pid is negative\n");
 }
 else{
          long int a = syscall(318,3,"abc.txt");
